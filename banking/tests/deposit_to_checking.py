0bd803c2e3a4f20c0c8f9a3f519b0804ba2cb69d
valhuber <valjhuber@gmail.com>
2020-10-06 07:44:15 -0700

    fix PythonPath, but fails per missing parent (Checking)

--- banking/tests/deposit_to_checking.py
+++ banking/tests/deposit_to_checking.py
@@ -1,3 +1,35 @@
+import os
+import sys
+from datetime import datetime
+
+cwd = os.getcwd()   # eg, /Users/val/python/pycharm/logic-bank/nw/tests
+required_path_python_rules = cwd  # seeking /Users/val/python/pycharm/Logic-Bank
+required_path_python_rules = required_path_python_rules.replace("/banking/tests", "")
+required_path_python_rules = required_path_python_rules.replace("\banking\tests", "")
+
+sys_path = ""
+required_path_present = False
+for each_node in sys.path:
+    sys_path += each_node + "\n"
+    if each_node == required_path_python_rules:
+        required_path_present = True
+
+if not required_path_present:
+    print("Fixing path (so can run from terminal)")
+    sys.path.append(required_path_python_rules)
+else:
+    pass
+    print("NOT Fixing path (default PyCharm, set in VSC Launch Config)")
+
+run_environment_info = "Run Environment info...\n\n"
+run_environment_info += " Current Working Directory: " + cwd + "\n\n"
+run_environment_info += "sys.path: (Python imports)\n" + sys_path + "\n"
+run_environment_info += "From: " + sys.argv[0] + "\n\n"
+run_environment_info += "Using Python: " + sys.version + "\n\n"
+run_environment_info += "At: " + str(datetime.now()) + "\n\n"
+
+print("\n" + run_environment_info + "\n\n")
+
 import datetime
 import banking.db.models as models
 from banking.logic import session  # opens db, activates logic listener <--
